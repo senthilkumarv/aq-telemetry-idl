a58a17f260caeda7e05fe90c802f22cc8df7b2a8
Senthil V Kumar <senthil.vijay.kumar@gmail.com>
2025-12-27 23:57:13 -0500

    Add root-level Package.swift for Xcode SPM discovery

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "TelemetryThrift",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(name: "TelemetryThrift", targets: ["TelemetryThrift"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apache/thrift.git", from: "0.20.0")
+    ],
+    targets: [
+        .target(
+            name: "TelemetryThrift",
+            dependencies: [
+                .product(name: "Thrift", package: "thrift")
+            ],
+            path: "packages/swift/Sources/TelemetryThrift"
+        )
+    ]
+)
+
