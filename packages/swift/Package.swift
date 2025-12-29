d8d92ab151c69d93c57c6d20e6ed5c1618f5b73f
Senthil V Kumar <senthil.vijay.kumar@gmail.com>
2025-12-28 23:16:24 -0500

    Remove packages

--- packages/swift/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:5.7
-import PackageDescription
-let package = Package(
-  name: "TelemetryThrift",
-  platforms: [.iOS(.v15)],
-  products: [ .library(name: "TelemetryThrift", targets: ["TelemetryThrift"]) ],
-  dependencies: [ .package(url: "https://github.com/apache/thrift.git", from: "0.20.0") ],
-  targets: [ .target(name: "TelemetryThrift", dependencies: [ .product(name: "Thrift", package: "thrift") ], path: "Sources/TelemetryThrift") ]
-)
